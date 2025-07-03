f3f99301fb929f363a85ded458a09714b086351d
Astraxx04 <gagan200254@gmail.com>
2025-07-03 09:44:00 +0530

    brought back data fetching during build

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,7 +1,6 @@
-const isDev = process.env.ELEVENTY_ENV === 'dev'
+const isDev = process.env.ELEVENTY_ENV === "dev";
 
 module.exports = {
-  baseUrl: isDev
-    ? '/'
-    : '/2025/'
-}
+	baseUrl: isDev ? "/" : "/2025/",
+	spreadsheetURL: "https://docs.google.com/spreadsheets/d/1oyMFGGKmR6u5JVpd691iYD_6TuDi3pbvyaajAZfGkGw/gviz/tq?tqx=out:json"
+};
