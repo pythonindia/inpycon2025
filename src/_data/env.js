b6ac4514fcb2a8f2a64e098da042f5f51a392d32
Astraxx04 <gagan200254@gmail.com>
2025-08-27 11:54:22 +0530

    Job Board Section

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,7 +1,6 @@
-const isDev = process.env.ELEVENTY_ENV === 'dev'
+const isDev = process.env.ELEVENTY_ENV === "dev";
 
 module.exports = {
-  baseUrl: isDev
-    ? '/'
-    : '/2025/'
-}
+	baseUrl: isDev ? "/" : "/2025/",
+	spreadsheetURL: "https://docs.google.com/spreadsheets/d/1gIGszJWFngjaP4GuEqudAuYg95LGRLm-xO6re-DIKMA/gviz/tq?tqx=out:json"
+};
